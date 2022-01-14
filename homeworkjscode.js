dbed10530730d34048a41f5f3afe963ef97d06dc
augustomay <90149173+augustomay@users.noreply.github.com>
2022-01-14 18:01:38 +0200

    Update homeworkjscode.js

--- homeworkjscode.js
+++ homeworkjscode.js
@@ -12,24 +12,27 @@ const create = function () {
         newDiv.appendChild(newPar);
         main.appendChild(newDiv).setAttribute("id", "div_" + i);   
 
+        if (i < 1 || i > 100) {
+            newDiv.style = "display: none";
+        } else {
             if (i % 3 === 0 && i % 5 === 0) {                              
                 newPar.appendChild(document.createTextNode(i + ' FIZZ BUZZ'));
                 main.appendChild(newDiv).setAttribute("class", "fizzbuzz");
 
-                } else if (i % 3 === 0) {
+            } else if (i % 3 === 0) {
                 newPar.appendChild(document.createTextNode(i + ' FIZZ'));
                 main.appendChild(newDiv).setAttribute("class", "fizz");
 
-                } else if (i % 5 === 0) {
+            } else if (i % 5 === 0) {
                 newPar.appendChild(document.createTextNode(i + ' BUZZ'));
                 main.appendChild(newDiv).setAttribute("class", "buzz");
 
-                } else {
+            } else {
                 newPar.appendChild(document.createTextNode(i));
                 main.appendChild(newDiv).setAttribute("class", "num");
-                }
             }
-    
+        }
+    }
 };    
 
 create();
